d35b84258787c0e8bea52128a877865a1a5dce6b
Nathanael Woodbridge <nate@logicspot.com>
2017-06-21 15:35:15 +0100

    Stop deploy task running inheritance twice, and bump version to 2.1.3

--- task/deploy.js
+++ task/deploy.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = function(gulp, config, plugins) { // eslint-disable-line func-names
    return () => {
-      // Tasks to run when deploying to server
-      plugins.runSequence('styles', 'scripts');
+		// Tasks to run when deploying to server
+		plugins.util.env.pipeline = true;
+
+		plugins.runSequence('inheritance', 'scripts', 'styles');
    };
 };
