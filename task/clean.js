57e1334f499a909335ead402e524450df7c66d10
Nate <hi@natewoodbridge.com>
2018-01-10 17:23:33 +0000

    Tweak the list of clean files and bump version to 2.3.2

--- task/clean.js
+++ task/clean.js
@@ -4,8 +4,9 @@ module.exports = function(gulp, config, plugins) { // eslint-disable-line func-n
 
       // Remove all files under pub/static, except .htaccess
       return gulp.src([
-         config.projectPath + 'pub/static/*',
+         config.projectPath + 'pub/static/frontend/*',
          config.projectPath + 'var/view_preprocessed/*',
+         config.projectPath + 'var/cache/*',
          '!' + config.projectPath + 'pub/static/.htaccess',
       ], { read: false })
       .pipe(plugins.rimraf({ force: true }));
