6d467a8b13ea658e1263921e4fbf56c4d73c5d45
Nate <hi@natewoodbridge.com>
2018-01-11 11:58:53 +0000

    Add another folder to clean task

--- task/clean.js
+++ task/clean.js
@@ -2,14 +2,15 @@
 module.exports = function(gulp, config, plugins) { // eslint-disable-line func-names
    return () => {
 
-      // Remove all files under pub/static, except .htaccess
-      return gulp.src([
-         config.projectPath + 'pub/static/frontend/*',
-         config.projectPath + 'var/view_preprocessed/*',
-         config.projectPath + 'var/cache/*',
-         '!' + config.projectPath + 'pub/static/.htaccess',
-      ], { read: false })
-      .pipe(plugins.rimraf({ force: true }));
-   
+    // Remove all files under pub/static, except .htaccess
+    return gulp.src([
+            config.projectPath + 'var/view_preprocessed/*',
+            config.projectPath + 'var/cache/*',
+            config.projectPath + 'pub/static/*',
+            '!' + config.projectPath + 'pub/static/adminhtml/*',
+            '!' + config.projectPath + 'pub/static/.htaccess'
+        ], { read: false })
+        .pipe(plugins.rimraf({ force: true }));
+    
    }
 };
